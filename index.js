27370fb75da243d8976db05bf4b56ff5941afa8d
Hideki Abe <hideki.abe.0630@gmail.com>
2015-01-20 17:39:18 +0900

    Remove basepath conditional branch
     include virtual isn't collect behavior

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ SSI.prototype = {
             function insertInclude(next) {
                 seg = matches[0];
                 isVirtual = RegExp.$1 == 'virtual';
-                basePath = (isVirtual && options.dirname)? options.dirname : options.baseDir;
+                basePath = options.baseDir;
                 tpath = path.join(basePath, RegExp.$3);
                 fs.readFile(tpath, {
                         encoding: options.encoding
